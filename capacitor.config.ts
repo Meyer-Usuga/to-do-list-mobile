4c0b8723e7d93188d8969e9241471eb1f80297f1
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2026-08-07 17:02:03 -0500

    feat: add Capacitor Android setup, Firebase config, and update app id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.meyerusuga.todolist',
   appName: 'to-do-list-mobile',
   webDir: 'www'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrCbUNchH9vWT5qAtKNJxw4opMw3vpruE",
+    authDomain: "to-do-list-mobile-f006d.firebaseapp.com",
+    projectId: "to-do-list-mobile-f006d",
+    storageBucket: "to-do-list-mobile-f006d.firebasestorage.app",
+    messagingSenderId: "578848287396",
+    appId: "1:578848287396:web:70159d939da76af2e61431"
+  }
 };
